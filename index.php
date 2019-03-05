ba132d48db368980b974cfcadf39ee8ffb0888eb
sachinthakur9614 <45144620+sachinthakur9614@users.noreply.github.com>
2019-03-06 01:11:56 +0530

    Add files via upload
    addded the project contribution

--- index.php
+++ index.php
@@ -22,13 +22,13 @@
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
   </ol>
   <div class="carousel-inner">
-    <div class="carousel-item active" data-interval="10000">
+    <div class="carousel-item active" data-interval="1000">
       <img src="IMG1.jpg" class="d-block w-100" alt="...">
     </div>
-    <div class="carousel-item" data-interval="10000">
+    <div class="carousel-item" data-interval="1000">
       <img src="IMG3.jpg" class="d-block w-100" alt="...">
     </div>
-    <div class="carousel-item" data-interval="10000">
+    <div class="carousel-item" data-interval="1000">
       <img src="IMG2.jpg" class="d-block w-100" alt="...">
     </div>
   </div>
@@ -41,6 +41,83 @@
     <span class="sr-only">Next</span>
   </a>
 </div>
+
+
+
+
+<div class="container">
+	
+	<hr>
+<div class="row">
+<div class="col-md-6">
+	<h1 class="text-center text-dark"> </h1>
+	<h5></h5>
+
+
+	<div class="card">
+  <div class="card-header">
+    Contribution:
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Lok Jumbish Project:</h5>
+    <p class="card-text">
+    The Lok Jumbish (LJ) project has 75 blocks covering approximately 12 million of population. LJ works hand in hand with government agencies, teachers, NGOs, elected representatives and the people in an interactive group effort to promote universalization of primary education. It works on seven guiding principles. These are:</p>
+A process rather than a product approach.
+  	<ul>
+
+  		
+  		<li>Partnerships.</li>
+  		<li>Decentralized functioning.</li>
+  		<li>Participatory learning.</li>
+
+  		<li>Integration with the mainstream education system.</li>
+  		<li> Flexibility of management.</li>
+  		<li>Creating multiple levels of leadership committed to quality and mission mode.
+</li>
+  	</ul>
+
+ </div>
+</div>
+
+	</div>
+
+	<div class="col-md-6">
+	<h1 class="text-center text-dark"> </h1>
+	<h5></h5>
+
+
+	<div class="card">
+  <div class="card-header">
+    Contribution:
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Lok Jumbish Project:</h5>
+    <p class="card-text">
+    The Lok Jumbish (LJ) project has 75 blocks covering approximately 12 million of population. LJ works hand in hand with government agencies, teachers, NGOs, elected representatives and the people in an interactive group effort to promote universalization of primary education. It works on seven guiding principles. These are:</p>
+A process rather than a product approach.
+  	<ul>
+
+  		
+  		<li>Partnerships.</li>
+  		<li>Decentralized functioning.</li>
+  		<li>Participatory learning.</li>
+
+  		<li>Integration with the mainstream education system.</li>
+  		<li> Flexibility of management.</li>
+  		<li>Creating multiple levels of leadership committed to quality and mission mode.
+</li>
+  	</ul>
+
+ </div>
+</div>
+
+	</div>
+
+
+
+
+
+
 
 
 <?php include "footer.php" ?>
