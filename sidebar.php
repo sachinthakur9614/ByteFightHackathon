6a4e45df4eef6adbd7cea43958dff5522e6f51b9
sachinthakur9614 <45144620+sachinthakur9614@users.noreply.github.com>
2019-03-06 10:36:20 +0530

    Add files via upload

--- sidebar.php
+++ sidebar.php
@@ -10,15 +10,14 @@
 
 
 
-<div  class="float-left">
-	<div class=" main_container padding col-lg-2 col-md-3 col-sm-3 col-xs-12" style="padding:0px;">
+	<div class=" main_container padding col-lg-2 col-md-3 col-sm-3 col-xs-6" style="padding:0px;">
 	<a href="index.php" class="btn btn-dark btn-lg" style="width: 100%;border-radius: 0;"><i class="fa fa-dashbaord"></i>Dashboard</a>
 	
 	<a href="course.php" class="btn btn-dark btn-lg" style="width: 100%;border-radius: 0;">Course enrolled</a>
 	
 	<a href="tutors.php" class="btn btn-dark btn-lg" style="width: 100%;border-radius: 0;">Tutors</a>
 	
-	<a href="projects.php" class="btn btn-dark btn-lg" style="width: 100%;border-radius: 0;">Projects</a>
+	
 	
 	<a href="Dance.php" class="btn btn-dark btn-lg" style="width: 100%;border-radius: 0;">Dance</a>
 	
@@ -26,7 +25,4 @@
 	
 	<a href="docs.php" class="btn btn-dark btn-lg" style="width: 100%;border-radius: 0;">Documentation</a>
 	
-</div>
-
-
 </div>
